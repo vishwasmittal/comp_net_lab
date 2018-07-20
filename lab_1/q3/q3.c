05db54b57faaf36c0981337aae3cce85d2650b8e
vishwas mittal <vishwas.mittal78@gmail.com>
2018-07-20 23:15:00 +0530

    added lab 1, q3

---
+++ lab_1/q3/q3.c
@@ -0,0 +1,15 @@
+#include "bits/stdc++.h"
+#include "unistd.h"
+using namespace std;
+
+int main() {
+	cout<<"process id "<<getpid()<<endl;
+	for(int i =0; i<5; i++){
+		pid_t pid = fork();
+		if(pid==0){
+			cout<<"process id "<<getpid()<<endl;
+			//fflush(stdout);
+		}
+	}
+	return 0;
+}	
